9d69baafdf1d1777521f0d6fa21b861c579c7d3e
Midgard Almeida <malmeidamdam1@gmail.com>
2024-04-30 13:36:16 +0200

    Primera versión nuevo menú realizada

--- ProyectoBiblioteca/MainWindow.xaml.cs
+++ ProyectoBiblioteca/MainWindow.xaml.cs
@@ -15,160 +15,20 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace ProyectoBiblioteca
 {
     public partial class MainWindow : Window
     {
-        private BibliotecaModel bbdd;
+        //private BibliotecaModel bbdd;
 
         public MainWindow()
         {
             InitializeComponent();
-            WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            bbdd = new BibliotecaModel();
-            gridResultados.MouseDoubleClick += MostrarDetallesObra;
+            //WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            //bbdd = new BibliotecaModel();
 
         }
-
-        private void buscarLibro(object sender, RoutedEventArgs e)
-        {
-            string textoBusqueda = txtBusqueda.Text.Trim();
-
-            if (!string.IsNullOrWhiteSpace(textoBusqueda) && textoBusqueda.Length > 1)
-            {
-                var librosEncontrados = bbdd.Libros
-                    .Where(libro =>
-                    libro.ISBN == textoBusqueda ||
-                    libro.Titulo == (textoBusqueda) ||
-                    libro.Autor.Contains(textoBusqueda)
-                );
-
-                if (librosEncontrados.Any())
-                {
-                    var librosResultados = librosEncontrados.Select(libro =>
-                        new { ISBN = libro.ISBN, Título = libro.Titulo, Autor = libro.Autor, Existencias = libro.Existencias }
-                    ).ToList();
-                    gridResultados.ItemsSource = librosResultados;
-                }
-                else
-                {
-                    MessageBox.Show("No se encontró ningún libro con el título, ISBN o autor proporcionado.");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Ingrese un término de búsqueda váido para realizar la búsqueda.");
-            }
-        }
-
-
-        private void buscarPelicula(object sender, RoutedEventArgs e)
-        {
-            string textoBusqueda = txtBusqueda.Text.Trim();
-
-            if (!string.IsNullOrWhiteSpace(textoBusqueda) && textoBusqueda.Length > 1)
-            {
-                var peliculasResultados = bbdd.Peliculas
-                .Where(pelicula => pelicula.Titulo == textoBusqueda || pelicula.Director.Contains(textoBusqueda))
-                .Select(pelicula => new { Título = pelicula.Titulo, Director = pelicula.Director, Existencias = pelicula.Existencias })
-                .ToList();
-
-                if (peliculasResultados.Any())
-                {
-                    gridResultados.ItemsSource = peliculasResultados;
-                }
-                else
-                {
-                    MessageBox.Show("No se encontraron películas con el título o director proporcionado.");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Ingrese un término de búsqueda váido para realizar la búsqueda.");
-            }
-        }
-
-
-        private void MostrarAyuda_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("El parámetro admite:\n\nLibros: Título, ISBN, Autor\nPelículas: Título, Director", "Ayuda", MessageBoxButton.OK, MessageBoxImage.Question);
-        }
-
-
-        private void MostrarDetallesObra(object sender, MouseButtonEventArgs e)
-        {
-            if (gridResultados.SelectedItem != null)
-            {
-                dynamic obraSeleccionada = gridResultados.SelectedItem;
-                try
-                {
-                    if (obraSeleccionada.ISBN != null)
-                    {
-                        string isbn = obraSeleccionada.ISBN;
-
-                        var libroEncontrado = bbdd.Libros.FirstOrDefault(libro => libro.ISBN == isbn);
-                        if (libroEncontrado != null)
-                        {
-                            MostrarDetallesLibro(libroEncontrado);
-                        }
-                        else
-                        {
-                            MessageBox.Show("No se encontró el libro en la base de datos.");
-                        }
-                    }
-                }
-                catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException)
-                {
-                    //Si sale exception, no tiene ISBN, es película
-                    //Nota para obraSeleccionada debes usar los nombres que tienen en el dataGrid no en la clase Libros o Peliculas
-                    if (obraSeleccionada.Título != null)
-                    {
-                        string titulo = obraSeleccionada.Título;
-
-                        var peliculaEncontrada = bbdd.Peliculas.FirstOrDefault(pelicula => pelicula.Titulo == titulo);
-                        if (peliculaEncontrada != null)
-                        {
-                            // Mostrar detalles de la película
-                            MostrarDetallesPelicula(peliculaEncontrada);
-                        }
-                        else
-                        {
-                            MessageBox.Show("No se encontró la película en la base de datos.");
-                        }
-                    }
-                }
-            }
-        }
-
-
-        private void MostrarDetallesLibro(Libros libro)
-        {
-            StringBuilder detalles = new StringBuilder();
-            detalles.AppendLine("Detalles del libro:");
-            detalles.AppendLine($"Título: {libro.Titulo}");
-            detalles.AppendLine($"Autor: {libro.Autor}");
-            detalles.AppendLine($"Género: {libro.Genero}");
-            detalles.AppendLine($"Año: {libro.Anio}");
-            detalles.AppendLine($"Editorial: {libro.Editorial}");
-            detalles.AppendLine($"Existencias: {libro.Existencias}");
-
-            MessageBox.Show(detalles.ToString(), "Detalles del libro", MessageBoxButton.OK, MessageBoxImage.Information);
-        }
-
-        private void MostrarDetallesPelicula(Peliculas pelicula)
-        {
-            StringBuilder detalles = new StringBuilder();
-            detalles.AppendLine("Detalles de la película:");
-            detalles.AppendLine($"Título: {pelicula.Titulo}");
-            detalles.AppendLine($"Director: {pelicula.Director}");
-            detalles.AppendLine($"Género: {pelicula.Genero}");
-            detalles.AppendLine($"Año: {pelicula.Anio}");
-            detalles.AppendLine($"Duración: {pelicula.Duracion} minutos");
-            detalles.AppendLine($"Existencias: {pelicula.Existencias}");
-
-            MessageBox.Show(detalles.ToString(), "Detalles de la película", MessageBoxButton.OK, MessageBoxImage.Information);
-        }
-
 
     }
 }
---
+++ ProyectoBiblioteca/NavButton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ProyectoBiblioteca
+{
+    public class NavButton : ListBoxItem
+    {
+        static NavButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NavButton), new FrameworkPropertyMetadata(typeof(NavButton)));
+        }
+
+        public Uri Navlink
+        {
+            get { return (Uri)GetValue(NavlinkProperty); }
+            set { SetValue(NavlinkProperty, value); }
+        }
+        public static readonly DependencyProperty NavlinkProperty = DependencyProperty.Register("Navlink", typeof(Uri), typeof(NavButton), new PropertyMetadata(null));
+
+        public ImageSource Icon
+        {
+            get { return (ImageSource)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(ImageSource), typeof(NavButton), new PropertyMetadata(null));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(NavButton), new PropertyMetadata(""));
+    }
+}
